4907c50a61a3ac47db69d2480b12d8810636f025
Lukáš Laincz <lukas.laincz@student.tuke.sk>
2022-02-17 23:10:05 +0100

    main window

---
+++ main_window.py
@@ -0,0 +1,143 @@
+from tkinter import *
+
+
+class Window:
+    def __init__(self):
+        self.window = Tk()
+        self.window.geometry("1024x500")
+        self.window.configure(bg = "#2a2b2a")
+        self.window.title("Reinforcement Learning Visualizator")
+        self.window.iconbitmap("icon.ico")
+        self.window.resizable(False, False)
+
+        self.canvas = Canvas(
+            self.window,
+            bg = "#2a2b2a",
+            height = 500,
+            width = 1024,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge")
+        self.canvas.place(x = 0, y = 0)
+
+        self.show_menu_window()
+
+    def show_menu_window(self):
+        self.text_intro = self.canvas.create_text(
+            78.5, 144.5,
+            text = "Welcome",
+            fill = "#ffffff",
+            font = ("RobotoRoman-Bold", 15))
+
+    def show_main_window(self):
+        self.background_img = PhotoImage(file = f"background.png")
+        self.background = self.canvas.create_image(
+            613.5, 250.0,
+            image=self.background_img)
+
+        self.scale_speed = Scale(
+            from_=1,
+            to=100,
+            orient=HORIZONTAL,
+            length=70,
+            resolution=1,
+            bg = "#E4E4E4"
+        )
+        self.scale_speed.place(x = 117, y = 115)
+
+        self.scale_gamma = Scale(
+            from_=0,
+            to=1,
+            orient=HORIZONTAL,
+            length=70,
+            resolution=0.05,
+            bg = "#E4E4E4"
+        )
+        self.scale_gamma.place(x = 117, y = 180)
+
+        self.img0 = PhotoImage(file = f"img0.png")
+        self.b0 = Button(
+            image = self.img0,
+            borderwidth = 0,
+            highlightthickness = 0,
+            command = self.destroy_main_window,
+            relief = "flat")
+        self.b0.place(x = 64, y = 50, width = 110, height = 35)
+
+        self.img1 = PhotoImage(file = f"img1.png")
+        self.b1 = Button(
+            image = self.img1,
+            borderwidth = 0,
+            highlightthickness = 0,
+            command = self.destroy_main_window,
+            relief = "flat")
+        self.b1.place(x = 64, y = 347, width = 110, height = 35)
+
+        self.img2 = PhotoImage(file = f"img2.png")
+        self.b2 = Button(
+            image = self.img2,
+            borderwidth = 0,
+            highlightthickness = 0,
+            command = self.destroy_main_window,
+            relief = "flat")
+        self.b2.place(x = 19, y = 410, width = 200, height = 35)
+
+        self.text_speed = self.canvas.create_text(
+            78.5, 144.5,
+            text = "Speed:",
+            fill = "#ffffff",
+            font = ("RobotoRoman-Bold", 15))
+
+        self.text_gamma = self.canvas.create_text(
+            72.0, 209.5,
+            text = "Gamma:",
+            fill = "#ffffff",
+            font = ("RobotoRoman-Bold", 15))
+
+        self.text_algo = self.canvas.create_text(
+            119.0, 257.5,
+            text = "Value Iteration",
+            fill = "#ffffff",
+            font = ("RobotoRoman-Bold", 15))
+
+        self.text_state = self.canvas.create_text(
+            119.5, 305.5,
+            text = "Deterministic",
+            fill = "#ffffff",
+            font = ("RobotoRoman-Bold", 15))
+
+        self.grid_canvas = Canvas(
+            self.canvas,
+            bg = "#E4E4E4",
+            height=400,
+            width=400,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+        self.grid_canvas.place(x = 284, y = 50)
+
+        self.output_canvas = Canvas(
+            self.canvas,
+            bg = "#E4E4E4",
+            height=400,
+            width=250,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+        self.output_canvas.place(x = 729, y = 50)
+
+    def destroy_main_window(self):
+        self.canvas.delete('all')
+        self.b0.destroy()
+        self.b1.destroy()
+        self.b2.destroy()
+        self.grid_canvas.destroy()
+        self.output_canvas.destroy()
+        self.scale_gamma.destroy()
+        self.scale_speed.destroy()
+
+my_window = Window()
+
+my_window.window.mainloop()
