84b4cef28403c68f235f0a5d49a232bb8ed3c250
Lukáš Laincz <lukas.laincz@gmail.com>
2022-02-18 19:48:48 +0100

    validate inputs

--- main_window.py
+++ main_window.py
@@ -35,13 +35,14 @@ class Window:
         self.scale_size.place(x = 704, y = 210)
 
         self.options = ["Value Iteration", "Policy Iteration"]
-        self.items = StringVar(self.window)
-        self.items.set(self.options[0])
-        self.dropdown = OptionMenu(self.window, self.items, *self.options)
+        self.algo = StringVar(self.window)
+        self.algo.set(self.options[0])
+        self.dropdown = OptionMenu(self.window, self.algo, *self.options)
         self.dropdown.config(width=15)
         self.dropdown.place(x = 704, y = 265)
 
-        self.check = Checkbutton(bg = "#E4E4E4")
+        self.stochastic = IntVar()
+        self.check = Checkbutton(bg = "#E4E4E4", variable=self.stochastic)
         self.check.place(x = 704, y = 310)
 
 
@@ -51,6 +52,12 @@ class Window:
         self.check.destroy()
         self.dropdown.destroy()
         self.scale_size.destroy()
+
+        if self.stochastic.get() == 0: 
+            self.state = "Deterministic"
+        else:
+            self.state = "Stochastic"
+
         self.show_setup_window()
 
 
@@ -58,35 +65,41 @@ class Window:
         self.background_img = PhotoImage(file = f"img/background_main.png")
         self.background = self.canvas.create_image(613.5, 250.0, image=self.background_img)
 
-        self.text_algo = self.canvas.create_text(119.0, 247.5, text = "Value Iteration", fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
-        self.text_state = self.canvas.create_text(118.5, 295.5, text = "Deterministic", fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
+        self.options = ["Agent", "Goal", "Trap", "Obstacle"]
+        self.item = StringVar(self.window)
+        self.item.set("Choose Item")
+        self.dropdown = OptionMenu(self.window, self.item, *self.options)
+        self.dropdown.config(width=15)
+        self.dropdown.place(x = 60, y = 80)
+
+        self.text_algo = self.canvas.create_text(119.0, 217.5, text = self.algo.get(), fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
+        self.text_state = self.canvas.create_text(118.5, 265.5, text = self.state, fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
 
         self.img0 = PhotoImage(file = f"img/img4.png")
         self.b0 = Button(image = self.img0, borderwidth = 0, highlightthickness = 0, command=self.validate_setup_inputs, relief = "flat")
-        self.b0.place(x = 20, y = 359, width = 197, height = 35)
+        self.b0.place(x = 20, y = 319, width = 197, height = 35)
 
-        self.options = ["Agent", "Goal", "Trap", "Obstacle"]
-        self.items = StringVar(self.window)
-        self.items.set("Choose Item")
-        self.dropdown = OptionMenu(self.window, self.items, *self.options)
-        self.dropdown.config(width=15)
-        self.dropdown.place(x = 60, y = 60)
+        self.canvas_grid = Canvas(self.canvas, bg = "#E4E4E4", height=400, width=400, bd = 0, highlightthickness = 0, relief = "ridge")
+        self.canvas_grid.place(x = 284, y = 50)
+
+        self.canvas_output = Canvas(self.canvas, bg = "#E4E4E4", height=400, width=250, bd = 0, highlightthickness = 0, relief = "ridge")
+        self.canvas_output.place(x = 729, y = 50)
         
 
     def validate_setup_inputs(self):
+        #TODO check whether agent and goal are present
         self.destroy_setup_window()
 
 
     def destroy_setup_window(self):
-        self.canvas.delete('all')
+        self.canvas.delete(self.text_algo)
+        self.canvas.delete(self.text_state)
         self.b0.destroy()
         self.dropdown.destroy()
         self.show_main_window()
 
 
     def show_main_window(self):
-        self.background = self.canvas.create_image(613.5, 250.0, image=self.background_img)
-
         self.scale_speed = Scale(from_=1, to=100, orient=HORIZONTAL, length=70, resolution=1, bg = "#E4E4E4")
         self.scale_speed.place(x = 117, y = 115)
 
@@ -107,14 +120,8 @@ class Window:
 
         self.text_speed = self.canvas.create_text(78.5, 144.5, text = "Speed:", fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
         self.text_gamma = self.canvas.create_text(72.0, 209.5, text = "Gamma:", fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
-        self.text_algo = self.canvas.create_text(119.0, 257.5, text = "Value Iteration", fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
-        self.text_state = self.canvas.create_text(119.5, 305.5, text = "Deterministic", fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
-
-        self.grid_canvas = Canvas(self.canvas, bg = "#E4E4E4", height=400, width=400, bd = 0, highlightthickness = 0, relief = "ridge")
-        self.grid_canvas.place(x = 284, y = 50)
-
-        self.output_canvas = Canvas(self.canvas, bg = "#E4E4E4", height=400, width=250, bd = 0, highlightthickness = 0, relief = "ridge")
-        self.output_canvas.place(x = 729, y = 50)
+        self.text_algo = self.canvas.create_text(119.0, 257.5, text = self.algo.get(), fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
+        self.text_state = self.canvas.create_text(119.5, 305.5, text = self.state, fill = "#ffffff", font = ("RobotoRoman-Bold", 15))
 
 
     def destroy_main_window(self):
@@ -122,8 +129,8 @@ class Window:
         self.b0.destroy()
         self.b1.destroy()
         self.b2.destroy()
-        self.grid_canvas.destroy()
-        self.output_canvas.destroy()
+        self.canvas_grid.destroy()
+        self.canvas_output.destroy()
         self.scale_gamma.destroy()
         self.scale_speed.destroy()
         self.show_intro_window()
