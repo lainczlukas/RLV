6622a86bc016689994e3b32bc4cdff1a9c849f3d
Lukáš Laincz <lukas.laincz@gmail.com>
2022-05-07 16:56:04 +0200

    PI math rendering

--- Policy_Iteration.py
+++ Policy_Iteration.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from Enums import Actors
+from Enums import Actors, Directions
 from Environment import Environment
 
 class Policy_Iteration:   
@@ -9,8 +9,6 @@ class Policy_Iteration:
 
         self.grid_size = environment.grid_size
         self.grid_actors = environment.grid_actors
-
-        self.Q = {}
 
         self.gamma = 0.8
         self.epsilon = 0.5
@@ -44,9 +42,22 @@ class Policy_Iteration:
             for x in range(self.grid_size):
                 for y in range(self.grid_size):
                     if self.grid_actors[x,y] != Actors.goal and self.grid_actors[x,y] != Actors.monster:
+                        self.environment.equations['{}{}'.format(x,y)] = []
                         action = self.environment.policy[x,y]
-                        self.environment.V[x,y] = sum([self.environment.P[x, y, action, x1, y1] * (self.environment.R[x1, y1] + self.gamma * self.environment.V[x1, y1]) for x1 in range(self.grid_size) for y1 in range(self.grid_size)])
+                        equation = "V(s{}{},{}) = ".format(x,y,str(Directions(action).name))
+                        state_value = 0
 
+                        for x1 in range(self.grid_size):
+                            for y1 in range(self.grid_size):
+                                state_value += self.environment.P[x, y, action, x1, y1] * (self.environment.R[x1, y1] + self.gamma * self.environment.V[x1, y1])
+                                if self.environment.P[x, y, action, x1, y1] != 0:
+                                    equation += "{} * ({} + {}  * {}) + \n".format(round(self.environment.P[x, y, action, x1, y1], 2), self.environment.R[x1, y1], self.gamma, round(self.environment.V[x1, y1], 2))                            
+                        self.environment.V[x,y] = state_value
+                        equation = equation[:-3]
+                        equation += "= {}".format(round(state_value,2))
+                        self.environment.equations['{}{}'.format(x,y)].append(equation)
+
+            
             if all(abs(old_values[x,y] - self.environment.V[x,y]) < self.epsilon for x in range(self.grid_size) for y in range(self.grid_size)):
                 break
 
@@ -55,16 +66,17 @@ class Policy_Iteration:
             for x in range(self.grid_size):
                 for y in range(self.grid_size):
                     if self.grid_actors[x,y] != Actors.goal and self.grid_actors[x,y] != Actors.monster:
+                        equation = "π(s{}{}) = argmax a (\n".format(x,y)
                         Q_values = []
                         for action in range(self.environment.N_actions):
-                            Q_values.append(sum(([self.environment.P[x, y, action, x1, y1] * (self.environment.R[x1, y1] + self.gamma * self.environment.V[x1, y1]) for x1 in range(self.grid_size) for y1 in range(self.grid_size)])))
-                        
-                        self.Q["{}{}".format(x,y)] = Q_values
+                            q_value = 0
+                            for x1 in range(self.grid_size):
+                                for y1 in range(self.grid_size):
+                                    q_value += self.environment.P[x, y, action, x1, y1] * (self.environment.R[x1, y1] + self.gamma * self.environment.V[x1, y1])
+                            
+                            equation += "{}, ".format(round(q_value,2))                        
+                            Q_values.append(q_value)
+
                         self.environment.policy[x,y] = np.argmax(Q_values)
-
-
-    def get_Q(self, x, y):
-        key = "{}{}".format(x,y)
-        if key in self.Q:
-            return self.Q[key]
-        return [0.0,0.0,0.0,0.0]
+                        equation += ") = {}".format(Directions(self.environment.policy[x,y]).name)
+                        self.environment.equations['{}{}'.format(x,y)].append(equation)
